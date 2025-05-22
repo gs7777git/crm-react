cac6d84fbc5651fdfdacd00e5bead2f987ae9c34
gs7777git <govindsaini355@gmail.com>
2025-05-22 16:59:35 +0000

    Pushing full app to GitHub

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import './index.css';
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css"; // Tailwind CSS import
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/crm-react">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
-);
-
+)
